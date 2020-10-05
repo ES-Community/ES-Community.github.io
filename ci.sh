7011f4706409b85de9a68b8595298a8b58855ab7
Purexo <contact@purexo.mom>
2020-10-05 23:55:45 +0200

    try with built-in auth

--- ci.sh
+++ ci.sh
@@ -7,7 +7,7 @@ git config --global user.name "Github CI"
 
 npm run clear
 
-git_url="https://${CI_USER_TOKEN}@github.com/ES-Community/ES-Community.github.io"
+git_url="https://github.com/ES-Community/ES-Community.github.io.git"
 git clone -b master ${git_url} public
 
 npm run build
@@ -17,9 +17,4 @@ cd public
 git add .
 git commit -m "Github-CI - Site Updated - ${DATE}"
 
-git_push_res=$(git push origin master 2>&1)
-git_return_code=$?
-
-echo "${git_push_res}" | sed -r "s/${CI_USER_TOKEN}/\[masked\]/g"
-
-exit ${git_return_code}
+git push origin master
