71c200fd75f7f651ae9660ca7d945c44d2444969
Purexo <contact@purexo.mom>
2020-10-06 00:08:52 +0200

    use x-access-token

--- ci.sh
+++ ci.sh
@@ -3,8 +3,11 @@ DATE=`date +%Y-%m-%d:%H:%M:%S`
 
 npm run clear
 
-git_url="https://github.com/ES-Community/ES-Community.github.io.git"
-git clone -b master ${git_url} public
+git config --global user.email "github@github.com"
+git config --global user.name "Github-CI"
+
+git_url="https://x-access-token:${GITHUB_TOKEN}github.com/ES-Community/ES-Community.github.io.git"
+git clone -b master "${git_url}" public
 
 npm run build
 
