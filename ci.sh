8ec846765cbf842a389fbe2c3cb888472173ea25
Purexo <contact@purexo.mom>
2020-10-05 23:57:15 +0200

    use builtin author git config

--- ci.sh
+++ ci.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 DATE=`date +%Y-%m-%d:%H:%M:%S`
 
-# config git for commits
-git config --global user.email "github@github.com"
-git config --global user.name "Github CI"
-
 npm run clear
 
 git_url="https://github.com/ES-Community/ES-Community.github.io.git"
